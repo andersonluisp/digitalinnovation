912f58a6890e64d63a90414c9b176fbbed0ef3fb
andersonluisp <andersonluiscp@gmail.com>
2021-04-11 08:06:46 -0300

    Revert "created enum client type and test to it"
    This reverts commit df5e8ef9

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,6 +1,16 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa (
-        val nome: String,
-        val cpf: String
-        )
+class Pessoa {
+    var nome: String = "Anderson"
+    var cpf: String = "123.123.123-11"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+}
+
+fun main() {
+    val anderson = Pessoa()
+    println(anderson.pessoaInfo())
+}
