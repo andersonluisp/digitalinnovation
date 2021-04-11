df5e8ef9fbd63d8188da7c44606e60b1e91b0ad7
andersonluisp <andersonluiscp@gmail.com>
2021-04-11 08:04:19 -0300

    created enum client type and test to it

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Anderson"
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val anderson = Pessoa()
-    println(anderson.pessoaInfo())
-}
+abstract class Pessoa (
+        val nome: String,
+        val cpf: String
+        )
